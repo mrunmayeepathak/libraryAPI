cfb1db2ba339296a4fd0ddb4365ffc3bfe4ce9ce
mrunmayeepathak <pathakmrun@gmail.com>
2025-03-15 22:39:13 +0530

    not working after LibraryItem abstract, Inheritance required and creating DVD journal and book classes required

--- src/main/java/com/example/Library_Management_API/entities/Book.java
+++ src/main/java/com/example/Library_Management_API/entities/Book.java
@@ -1,4 +1,18 @@
 package com.example.Library_Management_API.entities;
 
-public class Book {
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book extends LibraryItem {
+ Long ISBN;
+ String author;
+
 }
--- src/main/java/com/example/Library_Management_API/repository/ItemRepository.java
+++ src/main/java/com/example/Library_Management_API/repository/ItemRepository.java
@@ -1,5 +1,6 @@
 package com.example.Library_Management_API.repository;
 
+import com.example.Library_Management_API.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.Library_Management_API.entities.LibraryItem;
 
